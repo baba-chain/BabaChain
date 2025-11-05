9b61b06572a09bfefcfc3d25e79cdf008eb9ae41
baba-chain <semrukfoundation@gmail.com>
2025-11-05 20:45:15 +0300

    Development

---
+++ confdefs.h
@@ -0,0 +1,60 @@
+/* confdefs.h */
+#define PACKAGE_NAME "BabaChain Core"
+#define PACKAGE_TARNAME "babachaincore"
+#define PACKAGE_VERSION "23.0.0"
+#define PACKAGE_STRING "BabaChain Core 23.0.0"
+#define PACKAGE_BUGREPORT "https://github.com/baba-chain/babachain/issues"
+#define PACKAGE_URL "https://babachain.org/"
+#define HAVE_CXX20 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+#define ENABLE_MINER 1
+#define HAVE_CLMUL 1
+#define ENABLE_SSSE3 1
+#define ENABLE_SSE41 1
+#define ENABLE_AVX2 1
+#define ENABLE_X86_SHANI 1
+#define ENABLE_X86_AESNI 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+#define HAVE_PTHREAD 1
+#define HAVE_DECL_STRERROR_R 1
+#define HAVE_STRERROR_R 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SYS_VMMETER_H 1
+#define HAVE_DECL_GETIFADDRS 1
+#define HAVE_DECL_FREEIFADDRS 1
+#define HAVE_DECL_FORK 1
+#define HAVE_DECL_SETSID 1
+#define HAVE_DECL_PIPE2 0
+#define HAVE_DEFAULT_VISIBILITY_ATTRIBUTE 1
+#define HAVE_THREAD_LOCAL 1
+#define HAVE_GETENTROPY_RAND 1
+#define HAVE_SYSCTL 1
+#define HAVE_FDATASYNC 0
+#define HAVE_O_CLOEXEC 1
+#define HAVE_SOCKADDR_UN 1
+#define HAVE_SYSTEM 1
+#define USE_SQLITE 1
+#define HAVE_BOOST /**/
+#define ENABLE_ZMQ 1
+#define HAVE_CONSENSUS_LIB 1
+#define ENABLE_WALLET 1
+#define USE_DBUS 1
+#define CLIENT_VERSION_MAJOR 23
+#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_IS_RELEASE false
+#define COPYRIGHT_YEAR 2025
+#define COPYRIGHT_HOLDERS "The %s developers"
+#define COPYRIGHT_HOLDERS_SUBSTITUTION "BabaChain Core"
+#define COPYRIGHT_HOLDERS_FINAL "The BabaChain Core developers"
