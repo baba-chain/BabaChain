8228d738c3a1cb2569a618d40f2c740e69e916d7
baba-chain <semrukfoundation@gmail.com>
2025-11-05 03:16:50 +0300

    Create fiatonramp.h

---
+++ src/rpc/fiatonramp.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 The BabaChain Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_RPC_FIATONRAMP_H
+#define BITCOIN_RPC_FIATONRAMP_H
+
+class CRPCTable;
+
+void RegisterFiatOnRampRPCCommands(CRPCTable &t);
+
+#endif // BITCOIN_RPC_FIATONRAMP_H
